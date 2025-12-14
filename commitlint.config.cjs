bad7f6305b66698b62b8d43257e0c233ccfefd17
mwimer1 <mwimer1@users.noreply.github.com>
2025-12-14 14:39:57 -0500

    chore(config): disable commitlint body-max-line-length rule

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -48,7 +48,7 @@ module.exports = {
     ],
     'subject-case': [2, 'never', ['pascal-case', 'upper-case']],
     'subject-max-length': [2, 'always', 100],
-    'body-max-line-length': [2, 'always', 72],
+    'body-max-line-length': [0], // Disabled - no character limit on body lines
     'footer-max-line-length': [2, 'always', 72],
   },
 };
